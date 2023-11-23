cacc8d9b913548a865dacd829f2a32e349338d83
pranyubala <pranewbala01@gmail.com>
2023-11-23 15:07:14 +0530

    changes

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
 
 var names = [];
-names[0] = "Josh";
-names[1] = "Alice";
-names[2] = "john";
-names[3] = "Bob";
-names[4] = " David";
-names[5] = "James";
-names[6] = "jasmine";
-names[7] = "Michael";
+names[0] = "Yaakov";
+names[1] = "John";
+names[2] = "Jen";
+names[3] = "Jason";
+names[4] = "Paul";
+names[5] = "Frank";
+names[6] = "Larry";
+names[7] = "Paula";
+names[8] = "Laura";
+names[9] = "Jim";
 
 for (var i = 0; i < names.length; i++) {
     if (names[i].charAt(0)=== 'j' || names[i].charAt(0)=== 'J') {
-        console.log("Goodbye " + names[i]);
+        console.log("Good bye " + names[i]);
     } else {
         console.log("Hello " + names[i]);
     }
 }
+
+
